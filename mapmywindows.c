334c8d7bed65d1fd593c148492f42aa2f6c0d2a9
Damon Harris <TheDcoder@protonmail.com>
2018-03-20 15:26:13 +0530

    Add basic code

--- mapmywindows.c
+++ mapmywindows.c
@@ -1,6 +1,10 @@
+#include <stddef.h>
 #include <stdlib.h>
 #include <xdo.h>
 
 int main(void) {
+	xdo_t *xdo_instance = xdo_new(NULL);
+	if (xdo_instance == NULL) return EXIT_FAILURE;
+	xdo_free(xdo_instance);
 	return EXIT_SUCCESS;
 }
