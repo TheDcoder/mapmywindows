eabcac8c8eabcb52b86ba70a7d0d1bead5de404b
Damon Harris <TheDcoder@protonmail.com>
2018-03-26 22:17:59 +0530

    Restore window in the same location

--- mapmywindows.c
+++ mapmywindows.c
@@ -8,9 +8,12 @@ int main(void) {
 	if (xdo_instance == NULL) return EXIT_FAILURE;
 	Window window_instance;
 	xdo_get_active_window(xdo_instance, &window_instance);
+	int window_x, window_y;
+	xdo_get_window_location(xdo_instance, window_instance, &window_x, &window_y, NULL);
 	xdo_unmap_window(xdo_instance, window_instance);
 	sleep(1);
 	xdo_map_window(xdo_instance, window_instance);
+	xdo_move_window(xdo_instance, window_instance, window_x, window_y);
 	xdo_free(xdo_instance);
 	return EXIT_SUCCESS;
 }
