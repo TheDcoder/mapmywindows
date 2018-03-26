f79cc8cf349b5c832b8a12d50c71872a8551fe17
Damon Harris <TheDcoder@protonmail.com>
2018-03-26 21:08:53 +0530

    PoC: Hide the active window for a second

--- mapmywindows.c
+++ mapmywindows.c
@@ -1,10 +1,16 @@
 #include <stddef.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <xdo.h>
 
 int main(void) {
 	xdo_t *xdo_instance = xdo_new(NULL);
 	if (xdo_instance == NULL) return EXIT_FAILURE;
+	Window window_instance;
+	xdo_get_active_window(xdo_instance, &window_instance);
+	xdo_unmap_window(xdo_instance, window_instance);
+	sleep(1);
+	xdo_map_window(xdo_instance, window_instance);
 	xdo_free(xdo_instance);
 	return EXIT_SUCCESS;
 }
