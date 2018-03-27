cba8c7bca2ed832403630241561f10091b8e23bf
Damon Harris <TheDcoder@protonmail.com>
2018-03-27 11:08:17 +0530

    Add comments and formatting

--- mapmywindows.c
+++ mapmywindows.c
@@ -4,16 +4,27 @@
 #include <xdo.h>
 
 int main(void) {
+	// Create a new xdo instance and establish connection with X display server
 	xdo_t *xdo_instance = xdo_new(NULL);
 	if (xdo_instance == NULL) return EXIT_FAILURE;
+	
+	// Get active window
 	Window window_instance;
 	xdo_get_active_window(xdo_instance, &window_instance);
+	
+	// Store the window location
 	int window_x, window_y;
 	xdo_get_window_location(xdo_instance, window_instance, &window_x, &window_y, NULL);
+	
+	// Hide the window for 1 second and show it again
 	xdo_unmap_window(xdo_instance, window_instance);
 	sleep(1);
 	xdo_map_window(xdo_instance, window_instance);
+	
+	// Restore the window's position
 	xdo_move_window(xdo_instance, window_instance, window_x, window_y);
+	
+	// Cleanup and exit
 	xdo_free(xdo_instance);
 	return EXIT_SUCCESS;
 }
