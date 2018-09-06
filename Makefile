66ec98e58cdcb993cc24367aaae46f78a9ea1ad8
Damon Harris (TheDcoder) <TheDcoder@protonmail.com>
2018-09-06 09:44:04 +0530

    Add keyboard macro/shortcut support

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ compiler = gcc
 program = mapmywindows
 
 $(program): $(program).c
-	$(compiler) -lxdo -Wall -o $(program) $(program).c
+	$(compiler) -lX11 -lxdo -Wall -o $(program) $(program).c
 
 install: $(program)
 	cp $(program) /usr/bin
--- mapmywindows.c
+++ mapmywindows.c
@@ -1,10 +1,18 @@
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <X11/Xlib.h>
 #include <xdo.h>
 
 int main(void) {
+	// Get the current X display
+	Display *display = XOpenDisplay(NULL);
+	if (display == NULL) {
+		puts("Failed to get current X display");
+		return EXIT_FAILURE;
+	}
+	
 	// Create a new xdo instance and establish connection with X display server
 	xdo_t *xdo_instance = xdo_new(NULL);
 	if (xdo_instance == NULL) {
@@ -12,23 +20,55 @@ int main(void) {
 		return EXIT_FAILURE;
 	};
 	
-	// Get active window
+	// Grab the keys (keyboard shortcuts/macros)
+	KeyCode hide_key = XKeysymToKeycode(display, XStringToKeysym("F7"));
+	KeyCode show_key = XKeysymToKeycode(display, XStringToKeysym("F8"));
+	KeyCode exit_key = XKeysymToKeycode(display, XStringToKeysym("F9"));
+	
+	Window root_window = DefaultRootWindow(display);
+	XGrabKey(display, hide_key, Mod1Mask, root_window, true, GrabModeAsync, GrabModeAsync);
+	XGrabKey(display, show_key, Mod1Mask, root_window, true, GrabModeAsync, GrabModeAsync);
+	XGrabKey(display, exit_key, Mod1Mask, root_window, true, GrabModeAsync, GrabModeAsync);
+	
+	// Declare variables to store information about window
 	Window active_window;
-	xdo_get_active_window(xdo_instance, &active_window);
-	
-	// Store the window location
 	int window_x, window_y;
-	xdo_get_window_location(xdo_instance, active_window, &window_x, &window_y, NULL);
+	bool hidden = false;
 	
-	// Hide the window for 1 second and show it again
-	xdo_unmap_window(xdo_instance, active_window);
-	sleep(1);
-	xdo_map_window(xdo_instance, active_window);
-	
-	// Restore the window's position
-	xdo_move_window(xdo_instance, active_window, window_x, window_y);
-	
-	// Cleanup and exit
-	xdo_free(xdo_instance);
-	return EXIT_SUCCESS;
+	// Wait for events (X event loop)
+	XEvent event;
+	KeyCode pressed_key;
+	while (true) {
+		XNextEvent(display, &event);
+		if (event.type != KeyPress) continue;
+		pressed_key = event.xkey.keycode;
+		if (pressed_key == hide_key) {
+			// Check if a window is already hidden
+			if (hidden) {
+				puts("A window is already hidden!");
+				continue;
+			}
+			// Store the active window
+			xdo_get_active_window(xdo_instance, &active_window);
+			// Store the window's position
+			xdo_get_window_location(xdo_instance, active_window, &window_x, &window_y, NULL);
+			// Hide the window
+			xdo_unmap_window(xdo_instance, active_window);
+			hidden = true;
+			printf("Hidden the active window %li\n", active_window);
+		} else if (pressed_key == show_key) {
+			// Restore window
+			xdo_map_window(xdo_instance, active_window);
+			// Restore the window's position
+			xdo_move_window(xdo_instance, active_window, window_x, window_y);
+			printf("Shown the hidden window %li\n", active_window);
+			// Clear the stored active window
+			hidden = false;
+		} else if (pressed_key == exit_key) {
+			// Cleanup and exit
+			xdo_free(xdo_instance);
+			puts("Exiting!");
+			return EXIT_SUCCESS;
+		}
+	}
 }
