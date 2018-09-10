48a4337e0d521eeb4a9c6168a6e8f3d93cbe99fe
Damon Harris (TheDcoder) <TheDcoder@protonmail.com>
2018-09-10 11:45:09 +0530

    Implement linked node list to allow flexible storage of windows

--- mapmywindows.c
+++ mapmywindows.c
@@ -5,6 +5,20 @@
 #include <X11/Xlib.h>
 #include <xdo.h>
 
+struct window_node {
+	Window wid;
+	int x;
+	int y;
+	struct window_node *prev;
+	struct window_node *next;
+};
+
+void hide_window(void);
+void show_window(void);
+
+xdo_t *xdo_instance;
+struct window_node *current_window = NULL;
+
 int main(void) {
 	// Get the current X display
 	Display *display = XOpenDisplay(NULL);
@@ -14,7 +28,7 @@ int main(void) {
 	}
 	
 	// Create a new xdo instance and establish connection with X display server
-	xdo_t *xdo_instance = xdo_new(NULL);
+	xdo_instance = xdo_new(NULL);
 	if (xdo_instance == NULL) {
 		puts("Failed to start xdo!");
 		return EXIT_FAILURE;
@@ -30,11 +44,6 @@ int main(void) {
 	XGrabKey(display, show_key, Mod1Mask, root_window, true, GrabModeAsync, GrabModeAsync);
 	XGrabKey(display, exit_key, Mod1Mask, root_window, true, GrabModeAsync, GrabModeAsync);
 	
-	// Declare variables to store information about window
-	Window active_window;
-	int window_x, window_y;
-	bool hidden = false;
-	
 	// Wait for events (X event loop)
 	XEvent event;
 	KeyCode pressed_key;
@@ -43,32 +52,66 @@ int main(void) {
 		if (event.type != KeyPress) continue;
 		pressed_key = event.xkey.keycode;
 		if (pressed_key == hide_key) {
-			// Check if a window is already hidden
-			if (hidden) {
-				puts("A window is already hidden!");
-				continue;
-			}
-			// Store the active window
-			xdo_get_active_window(xdo_instance, &active_window);
-			// Store the window's position
-			xdo_get_window_location(xdo_instance, active_window, &window_x, &window_y, NULL);
-			// Hide the window
-			xdo_unmap_window(xdo_instance, active_window);
-			hidden = true;
-			printf("Hidden the active window %li\n", active_window);
+			hide_window();
 		} else if (pressed_key == show_key) {
-			// Restore window
-			xdo_map_window(xdo_instance, active_window);
-			// Restore the window's position
-			xdo_move_window(xdo_instance, active_window, window_x, window_y);
-			printf("Shown the hidden window %li\n", active_window);
-			// Clear the stored active window
-			hidden = false;
+			show_window();
 		} else if (pressed_key == exit_key) {
 			// Cleanup and exit
 			xdo_free(xdo_instance);
 			puts("Exiting!");
 			return EXIT_SUCCESS;
 		}
+	}
+}
+
+void hide_window(void) {
+	if (current_window == NULL) {
+		// Create the first node in our window list
+		current_window = malloc(sizeof(struct window_node));
+		if (current_window == NULL) {
+			puts("Failed to allocate memory to store the first window!");
+			return;
+		}
+		current_window->prev = NULL;
+		current_window->next = NULL;
+	} else {
+		// Create and append a new node in our window list
+		current_window->next = malloc(sizeof(struct window_node));
+		if (current_window->next == NULL) {
+			puts("Failed to allocate memory to store the window!");
+			return;
+		}
+		current_window->next->prev = current_window;
+		current_window = current_window->next;
+		current_window->next = NULL;
+	}
+	// Store the active window
+	xdo_get_active_window(xdo_instance, &current_window->wid);
+	// Store the window's position
+	xdo_get_window_location(xdo_instance, current_window->wid, &current_window->x, &current_window->y, NULL);
+	// Hide the window
+	xdo_unmap_window(xdo_instance, current_window->wid);
+	printf("Hidden the active window %li\n", current_window->wid);
+}
+
+void show_window(void) {
+	if (current_window == NULL) {
+		puts("No windows are hidden!");
+		return;
+	}
+	// Restore window
+	xdo_map_window(xdo_instance, current_window->wid);
+	// Restore the window's position
+	xdo_move_window(xdo_instance, current_window->wid, current_window->x, current_window->y);
+	printf("Shown the hidden window %li\n", current_window->wid);
+	if (current_window->prev == NULL) {
+		// Delete the only node in our list and reset it
+		free(current_window);
+		current_window = NULL;
+	} else {
+		// Delete the current node and move the pointer to previous node
+		current_window = current_window->prev;
+		free(current_window->next);
+		current_window->next = NULL;
 	}
 }
